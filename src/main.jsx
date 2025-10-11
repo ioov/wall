336cd6f44288dc72c0e5adfcdde8ffd2e1dcd515
Yupao <suoovn@gmail.com>
2025-10-11 10:47:17 +0800

    更新路由基础名防止404

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import RotuerApp from '@/router/intercept.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	// <React.StrictMode>
-	<BrowserRouter>
+	<BrowserRouter basename="/wall">
 		<React.Suspense fallback={<Loading />}>
 			{/* <RouterProvider router={Routers} /> */}
 			<RotuerApp />
